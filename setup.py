5e8af6005e25da405eecf73026f1fc3d72cde897
laffra <laffra@gmail.com>
2021-11-09 13:05:10 +0100

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class CustomInstall(install):
 
 setup(
     name = 'pyinsights',
-    version = '0.1.12',
+    version = '0.1.13',
     packages = find_packages(),
     install_requires = REQUIRED_PACKAGES,
     include_package_data=True,
